6b31dde0949b060ee5aac0404a0e61efe601dabe
Melissa Lawrence <mlawrence537@gmail.com>
2021-05-20 15:33:20 -0400

    added toggle and form for new show

--- src/TourDetails.js
+++ src/TourDetails.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation} from 'react-router-dom'
 import SingleShow from './SingleShow';
 
 function TourDetails() {
   const location = useLocation()
+  const [addShowClicked, setAddShowClicked] = useState(false)
 
   const showsArr = location.state.params.shows.map(show => {
     return <SingleShow key={show.id} show={show}/>
   })
+
+  function handleAddShowClick() {
+    setAddShowClicked(addShowClicked => !addShowClicked)
+  }
   
     return (
       <div>
         <h1>{location.state.params.name}</h1>
         <img src={location.state.params.image} alt={location.state.params.name}/>
         {showsArr}
+        <div onClick={handleAddShowClick}>
+            <h4>Add a show!</h4>
+        </div>
+        {addShowClicked && 
+        <div>
+          <form>
+            <label>Date: </label>
+            <input type="text"></input>
+            <label>Location: </label>
+               <input type="text"></input>
+               <label>Time: </label>
+               <input type="text"></input>
+               <label>Venue: </label>
+               <input type="text"></input>
+               <label>Other bands: </label>
+               <input type="text"></input>
+               <label>Details: </label>
+               <input type="text"></input>
+               <input type="submit"></input>
+          </form>
+        </div>}
       </div>
     );
   }
