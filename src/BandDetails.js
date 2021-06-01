fbcdd3b04a3848b810281624018ebda2f8778de0
Melissa Lawrence <mlawrence537@gmail.com>
2021-06-01 13:48:50 -0400

    passed band info to details page

--- src/BandDetails.js
+++ src/BandDetails.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function BandDetails() {
-    
+    const location = useLocation()
+    console.log(location.state.params)
     return (
-        <div className="profileMainContainer">
-        <h1 className='bandProfileHeader'>name</h1>
+        <div className="bandProfileMainContainer">
+        <h1 className='bandProfileHeader'>{location.state.params.name}</h1>
         <div className="bandInfoDiv">
-            {/* <div className='bandProfileImgDiv'>
-                <img className='bandProfileImg' src={currentUser.image} alt={currentUser.name} />
-            </div> */}
+            <div className='bandProfileImgDiv'>
+                <img className='bandProfileImg' src={location.state.params.image} alt={location.state.params.name} />
+            </div>
             <div className='bandInfo'>
-                <p><span className="profSpan">Email: </span></p>
-                <p><span className="profSpan">Website: </span></p>
-                <p><span className="profSpan">Facebook: </span></p>
-                <p><span className="profSpan">Music: </span></p>
-                <p><span className="profSpan">Genre: </span></p>
-                <p><span className="profSpan">Location: </span></p>
-                <p><span className="profSpan">Band members: </span></p>
-                <p><span className="profSpan">Bio: </span></p>
+                <p><span className="profSpan">Email: </span>{location.state.params.email}</p>
+                <p><span className="profSpan">Website: </span>{location.state.params.website}</p>
+                <p><span className="profSpan">Facebook: </span>{location.state.params.facebook}</p>
+                <p><span className="profSpan">Music: </span>{location.state.params.music_link}</p>
+                <p><span className="profSpan">Genre: </span>{location.state.params.genre}</p>
+                <p><span className="profSpan">Location: </span>{location.state.params.location}</p>
+                <p><span className="profSpan">Band members: </span>{location.state.params.band_members}</p>
+                <p><span className="profSpan">Bio: </span>{location.state.params.bio}</p>
             </div>
         </div>
         </div>
--- src/SingleBand.js
+++ src/SingleBand.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
-function SingleBand({ band}) {
+function SingleBand({ band }) {
+    const history = useHistory()
+
+    function handleBandClick() {
+        history.push(`bands/${band.id}`, {params: band})
+    }
     
     return (
         <div className="bandCard">
@@ -9,7 +14,7 @@ function SingleBand({ band}) {
             <p>{band.location}</p>
             <p>{band.genre}</p>
             <img src={band.image} alt={band.name} />
-            <Link to={`bands/${band.id}`}>{band.name}</Link>
+            <button onClick={handleBandClick}>{band.name}</button>
       </div>
     );
   }
