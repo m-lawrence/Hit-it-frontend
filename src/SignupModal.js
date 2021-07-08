c6cafbc7131883e9f541064e3a08cdda5d7792e9
Melissa Lawrence <mlawrence537@gmail.com>
2021-07-08 11:48:41 -0400

    style sign up form modal

--- src/SignupModal.js
+++ src/SignupModal.js
@@ -65,27 +65,27 @@ function SignupModal({ setSigningUp, addNewBand }) {
               <div className="input-group">
                   <form onSubmit={handleSignUpSubmit}>
                    <label className="signupEmailLabel">Email: </label>
-                   <input className="signupNameInput" type="text" name="email" value={signUpData.email} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupNameInput" type="text" name="email" value={signUpData.email} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupPasswordLabel">Password: </label>
-                   <input className="signupPasswordInput" type="password" name="password" value={signUpData.password} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupPasswordInput" type="password" name="password" value={signUpData.password} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupNameLabel">Band name: </label>
-                   <input className="signupNameInput" type="text" name="name" value={signUpData.name} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupNameInput" type="text" name="name" value={signUpData.name} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupWebsiteLabel">Website: </label>
-                   <input className="signupWebsiteInput" type="text" name="website" value={signUpData.website} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupWebsiteInput" type="text" name="website" value={signUpData.website} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupFacebookLabel">Facebook: </label>
-                   <input className="signupFacebookInput" type="text" name="facebook" value={signUpData.facebook} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupFacebookInput" type="text" name="facebook" value={signUpData.facebook} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupMusicLinkLabel">Music link: </label>
-                   <input className="signupMusicLinkInput" type="text" name="music_link" value={signUpData.music_link} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupMusicLinkInput" type="text" name="music_link" value={signUpData.music_link} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupGenreLabel">Genre: </label>
-                   <input className="signupGenreInput" type="text" name="genre" value={signUpData.genre} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupGenreInput" type="text" name="genre" value={signUpData.genre} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupLocationLabel">Location: </label>
-                   <input className="signupLocationInput" type="text" name="location" value={signUpData.location} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupLocationInput" type="text" name="location" value={signUpData.location} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupImageLabel">Image URL: </label>
-                   <input className="signupImageInput" type="text" name="image" value={signUpData.image} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupImageInput" type="text" name="image" value={signUpData.image} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupBioLabel">Bio: </label>
-                   <input className="signupBioInput" type="textarea" name="bio" value={signUpData.bio} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupBioInput" type="textarea" name="bio" value={signUpData.bio} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <label className="signupBandMembersLabel">Band members: </label>
-                   <input className="signupBandMembersInput" type="textarea" name="band_members" value={signUpData.band_members} onChange={handleSignUpFormChange}></input><br></br>
+                   <input className="signupBandMembersInput" type="textarea" name="band_members" value={signUpData.band_members} onChange={handleSignUpFormChange}></input><br></br><br></br>
                    <button type="submit" className="bottom-btn">Sign up!</button>
                </form>
               </div>
